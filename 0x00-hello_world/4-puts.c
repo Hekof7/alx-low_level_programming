9c1c7d76bf3b675dbc1906b23d6f0bfe4681f279
hekof7 <hekof7@gmail.com>
2023-07-13 12:11:30 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - a program that prints a line
+ * main - Entry point
  *
- * return: 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

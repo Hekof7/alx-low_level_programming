8a96ab0ba4f38e5f1cc8bbecea64d660f4289d7f
hekof7 <hekof7@gmail.com>
2023-07-13 11:51:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a line using printf
+ * return: (0)
+ */
+int main(void)
+{
+	printf("ith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

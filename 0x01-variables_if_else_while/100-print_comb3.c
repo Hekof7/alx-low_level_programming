4b1f7de25176e553bd505e499389959748e8429e
hekof7 <hekof7@gmail.com>
2023-07-15 13:15:15 -0700

    Print Combination 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,10 +18,10 @@ int main(void)
 			{
 				putchar(n);
 				putchar(m);
-				if (n == 8 && m == 9)
-                                        {
-						break;
-					}
+				if (n == '8' && m == '9')
+				{
+					break;
+				}
 				if (n != '8' || (n == '8' && m != 9))
 				{
 					putchar(',');

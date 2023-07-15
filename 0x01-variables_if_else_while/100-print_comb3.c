5ae8b26de0bd91ef19a21041d6cdfe41c8f1c962
hekof7 <hekof7@gmail.com>
2023-07-15 10:44:48 -0700

    Print Combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Prints the possible compination of two number
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = n + 1; n <= 9)
+		{
+			if (n != m)
+			{
+				putchar(n);
+				putchar(m);
+			}
+			if (n == 8 && m == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}

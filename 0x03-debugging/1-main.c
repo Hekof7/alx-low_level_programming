6f671884ba20a7997d5984774afd3d4593ccf64d
hekof7 <hekof7@gmail.com>
2023-07-20 09:32:36 -0700

    Infinte Loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		printf("%d", i);
-		i++;
-	}
+	/*while while (i < 10)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

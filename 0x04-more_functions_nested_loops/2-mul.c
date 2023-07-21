dffe3029843e8ae01833f2732199901af64b7207
hekof7 <hekof7@gmail.com>
2023-07-21 06:53:49 -0700

    multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intigers
+ * @a: the character to be checked
+ * @b: the character to be checked
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

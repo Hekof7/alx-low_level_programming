ebd21b4b2917f8f6f6ce7750fa0489acd580802b
hekof7 <hekof7@gmail.com>
2023-07-21 12:52:11 -0700

    Print Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints n number of line
+ * @n: the character to be checked
+ * Return: always 0
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

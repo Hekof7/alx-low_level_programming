7c1ce2787780243142cad12ee890991a47f72f2c
hekof7 <hekof7@gmail.com>
2023-07-24 12:21:37 -0700

    prints avery character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to be prited
+ * Return: always 0
+ */
+
+#include <stdio.h>
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}

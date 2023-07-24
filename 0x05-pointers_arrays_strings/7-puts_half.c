b339084f5a9595a9881837220f325d5090b36deb
hekof7 <hekof7@gmail.com>
2023-07-24 12:45:27 -0700

    pring half of the aray

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string to be printed
+ * Return: always 0
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		len++;
+		ptr++;
+	}
+
+	int half_len = (len + 1) / 2;
+
+	ptr = str + half_len;
+
+	while (*ptr != '\0')
+	{
+		putchar(*ptr);
+		ptr++;
+	}
+	putchar('\n');
+}

5d216d459d6a9ad899b355f8907a4ddafc5ffd11
hekof7 <hekof7@gmail.com>
2023-08-05 07:31:45 -0700

    libmy.a

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the charater is whether a digit
+ * @c: the character to be checked
+ * Return: 1 if it is digit 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - concaenate two stings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+}

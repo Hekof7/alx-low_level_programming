e25b2b6a817d896519f5da6da9a8a2738d41f86e
hekof7 <hekof7@gmail.com>
2023-08-15 11:11:18 -0700

    testt

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,18 +8,15 @@
  * @owner: owner
  */
 
-struct dog
-{
-	char* name;
-	float age;
-	char* owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
 
 typedef struct dog dog_t;
+
+struct dog
+{
+	char  *name;
+	float age;
+	char  *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -27,5 +24,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-	
+
 #endif

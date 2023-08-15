903a784ced308e0f1b0141fb0b08eb1c83f735bb
hekof7 <hekof7@gmail.com>
2023-08-15 00:45:02 -0700

    dog.h1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,8 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * init_dog - initialize a variable
- * @d: pointer
+ * struct dog - initialize a variable
  * @name: name inirialization
  * @age: age
  * @owner: owner
@@ -16,11 +15,4 @@ struct dog
 	char* owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+#endif
